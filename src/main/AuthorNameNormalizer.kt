e92acbcbe06fb5b93da1a70b2326b7a91111e0cd
Craig B <cban0901@hotmail.com>
2022-08-31 08:31:21 +0100

    test 6 pass

--- src/main/AuthorNameNormalizer.kt
+++ src/main/AuthorNameNormalizer.kt
@@ -2,13 +2,13 @@ class AuthorNameNormalizer() {
     fun normalize(name: String): String {
         val splitName = name.trim().split(" ")
         if (splitName.count() == 1) return name
-        val dot = if (splitName.count() > 2) "." else ""
-        return "${splitName.last()}, ${splitName.first()}${middleName(splitName)}$dot"
+        return "${splitName.last()}, ${splitName.first()}${middleName(splitName)}"
     }
 
     private fun middleName(splitName: List<String>): String {
         var middleName = ""
-        if (splitName.count() > 2) middleName = " ${splitName[1].first()}"
+        val dot = if (splitName.count() > 2 && splitName[1].length > 1) "." else ""
+        if (splitName.count() > 2) middleName = " ${splitName[1].first()}$dot"
         return middleName
     }
 }
