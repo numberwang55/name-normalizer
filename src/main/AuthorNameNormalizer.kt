056975f0432f9e20062c56d775618f353a42359a
Craig B <cban0901@hotmail.com>
2022-08-31 08:41:43 +0100

    test 7 pass

--- src/main/AuthorNameNormalizer.kt
+++ src/main/AuthorNameNormalizer.kt
@@ -2,13 +2,19 @@ class AuthorNameNormalizer() {
     fun normalize(name: String): String {
         val splitName = name.trim().split(" ")
         if (splitName.count() == 1) return name
-        return "${splitName.last()}, ${splitName.first()}${middleName(splitName)}"
+        return "${splitName.last()}, ${splitName.first()}${middleNames(splitName)}"
     }
 
-    private fun middleName(splitName: List<String>): String {
-        var middleName = ""
-        val dot = if (splitName.count() > 2 && splitName[1].length > 1) "." else ""
-        if (splitName.count() > 2) middleName = " ${splitName[1].first()}$dot"
-        return middleName
+    private fun middleNames(splitName: List<String>): String {
+        val middleNames = splitName.drop(1).dropLast(1)
+        return buildString {
+            middleNames.forEach { middleName ->
+                append(initialiseName(middleName))
+            }
+        }
+    }
+
+    private fun initialiseName(splitName: String): String {
+        return " ${splitName.first()}."
     }
 }
